74bbe5e82a4703f2b56e9abf275af2a72bd1b0ec
miretz <1217795+Miretz@users.noreply.github.com>
2021-11-01 13:04:05 +0100

    Use the Concepts: rewrite factorial to use printf

--- chapter8/factorial.s
+++ chapter8/factorial.s
@@ -4,20 +4,49 @@
 #            4 is 4 * 3 * 2 * 1, or 24, and so on.
 
 .section __DATA,__data
+
+format: .asciz "The factorial of %d is %d.\n"
+
 .section __TEXT,__text
+
+.equ ST_ARGV_1, 8    # input argument
 
 .globl _main
 _main:
-    pushq $4        # the number input for the
+    dec %rdi         # check number of arguments
+    cmpq $1, %rdi    # we need exactly 1 argument
+    jne set_default  # wrong number of args, 
+                     # use default value
+
+    # align the stack - needed in 64 bit asm
+    and $-16, %rsp
+
+    movq ST_ARGV_1(%rsi), %rdi # read the argument
+    
+    # convert string argument to integer
+    callq _atoi
+    addq $8, %rsp
+
+    movq %rax, %rbx
+    jmp continue_factorial     # continue factorial
+
+set_default:
+    movq $4, %rbx   # set a default value
+
+continue_factorial:
+    pushq %rbx      # the number input for the
                     # factorial function
     call factorial  # run the factorial function
-    addq  $4, %rsp  # scrubs the parameter that was pushed
-                    # on the stack
-    movq %rax, %rdi # factorial returns the answer
-                    # in %rax, but we need it in %rdi
-                    # to send it as our exit status
+
+print_result:
+    # printf the result
+    leaq  format(%rip), %rdi
+    movq  %rbx, %rsi
+    movq  %rax, %rdx
+    movq  %rax, %rax    # cleanup
+    callq _printf
 
     movl $0x2000001,%eax
+    movl $0, %edi
     syscall
-
 
